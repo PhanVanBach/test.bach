2a712d9d816ae91f5fdc52d4cde8e5de47f3b754
Bach <bachphan154@.gmail.com>
2024-05-07 18:01:15 +0700

    Revert "test 2"
    This reverts commit f30c2739a046a4d0288f6c3c79274b6bdc3773af.

---
+++ src/ui/FormSearch.jsx
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: white;
+  position: relative;
+  border-radius: 16px;
+  margin-bottom: 25px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 56px;
+  border-radius: 8px;
+  padding: 8px 8px 8px 16px;
+  gap: 16px;
+  border: none;
+  outline: none;
+  &:focus {
+    outline: 1px solid rgba(211, 211, 211, 0.5); /* Thêm đường viền nhạt khi focus */
+  }
+  font-size: 14px;
+`;
+
+const ButtonSearch = styled.div`
+  position: absolute;
+  z-index: 100;
+  right: 0;
+  height: auto;
+  margin: 5px;
+`;
+
+const Search = styled.button`
+  background-color: #624bff;
+  width: 115px;
+  height: 34px;
+  border-radius: 8px;
+  padding: 6px 8px 6px 8px;
+  color: #fff;
+  cursor: pointer;
+  border: none;
+`;
+
+function FormSearch({ query, setQuery }) {
+  return (
+    <Container>
+      <Input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="search"
+        placeholder="Nhập tên dự án"
+      />
+      <ButtonSearch>
+        <Search>Tìm kiếm</Search>
+      </ButtonSearch>
+    </Container>
+  );
+}
+
+export default FormSearch;
